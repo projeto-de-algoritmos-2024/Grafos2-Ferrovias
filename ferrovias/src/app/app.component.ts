a2c050d20d183544356a8f378f82fa73740a5445
Pablo S. Costa <pablo0359@gmail.com>
2024-12-02 04:39:49 +0000

    aplica leitura de imgs

--- ferrovias/src/app/app.component.ts
+++ ferrovias/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { isPlatformBrowser } from '@angular/common';
 import { NoComponent } from './no/no.component';
+import { getPixels, savePixels } from 'ndarray-pixels';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,7 @@ import { NoComponent } from './no/no.component';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  constructor(
-    @Inject(PLATFORM_ID) private platformId: any
-  ) { }
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
   title = 'onda';
   maximodenos: any;
   larguraTela: any;
@@ -23,56 +22,57 @@ export class AppComponent {
   tempo = 10;
   grafo: any = [];
   populagrafo() {
-    const colunas = Math.trunc(this.larguraTela / (this.tamanhono));
-    const linhas = Math.trunc(this.alturaTela / (this.tamanhono));
+    const colunas = Math.trunc(this.larguraTela / this.tamanhono);
+    const linhas = Math.trunc(this.alturaTela / this.tamanhono);
     let cont = 0;
     for (let j = 0; j < linhas; j++) {
       for (let i = 0; i < colunas; i++) {
         this.grafo[cont] = [];
-        if(j != 0){
-          this.grafo[cont].push((cont - colunas));
+        if (j != 0) {
+          this.grafo[cont].push(cont - colunas);
         }
-        if(j != linhas - 1){
-          this.grafo[cont].push((cont + colunas));
+        if (j != linhas - 1) {
+          this.grafo[cont].push(cont + colunas);
         }
-        if(i != 0){
-          this.grafo[cont].push((cont - 1));
+        if (i != 0) {
+          this.grafo[cont].push(cont - 1);
         }
-        if(i != colunas - 1){
-          this.grafo[cont].push((cont + 1));
+        if (i != colunas - 1) {
+          this.grafo[cont].push(cont + 1);
         }
-        console.log(cont+", "+colunas+", "+linhas);
+        console.log(cont + ', ' + colunas + ', ' + linhas);
         cont++;
       }
     }
-    console.log("acabou");
+    console.log('acabou');
   }
 
-  buscaEmLargura(no: any){
+  buscaEmLargura(no: any) {
     var grafocopia = JSON.parse(JSON.stringify(this.grafo));
     console.log(grafocopia);
     console.log(no);
     let lista = [no];
     let time = this.tempo;
-    function busca(numerono: any){
+    function busca(numerono: any) {
       let noagora = document.getElementById(numerono.toString());
-      if(!grafocopia[numerono].includes("explorado")){
+      if (!grafocopia[numerono].includes('explorado')) {
         lista = lista.concat(grafocopia[numerono]);
-        grafocopia[numerono].push("explorado");
+        grafocopia[numerono].push('explorado');
         console.log(lista);
 
         if (noagora) {
-        noagora.children[0].classList.add("ligaonda")}
+          noagora.children[0].classList.add('ligaonda');
+        }
         setTimeout(() => busca(lista[0]), time);
         console.log(numerono);
-        lista = lista.filter(item => item != numerono);
-
-      }else{
-        lista = lista.filter(item => item != numerono);
+        lista = lista.filter((item) => item != numerono);
+      } else {
+        lista = lista.filter((item) => item != numerono);
         if (noagora) {
-          noagora.children[0].classList.remove("ligaonda")}
-        console.log("nó "+numerono+" explorado");
-        if(lista.length){
+          noagora.children[0].classList.remove('ligaonda');
+        }
+        console.log('nó ' + numerono + ' explorado');
+        if (lista.length) {
           busca(lista[0]);
         }
       }
@@ -80,16 +80,27 @@ export class AppComponent {
     busca(no);
   }
 
-  ngOnInit() {
+  async ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
-      this.larguraTela = window.innerWidth ;
-      this.alturaTela = window.innerHeight ;
+      this.larguraTela = window.innerWidth;
+      this.alturaTela = window.innerHeight;
       console.log(this.larguraTela);
       console.log(this.alturaTela);
     }
-    this.maximodenos = Math.trunc(this.larguraTela) * Math.trunc(this.alturaTela);
+    this.maximodenos =
+      Math.trunc(this.larguraTela) * Math.trunc(this.alturaTela);
     this.nos = (this.tamanhono - 5) / this.maximodenos;
     this.populagrafo();
 
+    if (isPlatformBrowser(this.platformId)) {
+      const bytesIn = await fetch(
+        'https://cdn-icons-png.flaticon.com/256/10751/10751558.png'
+      )
+        .then((res) => res.arrayBuffer())
+        .then((arrayBuffer) => new Uint8Array(arrayBuffer));
+
+      const pixels = await getPixels(bytesIn, 'image/png'); // Uint8Array -> ndarray
+      console.log('got pixels', pixels.shape.slice());
+    }
   }
 }
