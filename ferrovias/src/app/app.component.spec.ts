8677f5a633a80d6218fa130a306a8af45a7f26aa
pabloheika <pablo0359@gmail.com>
2024-12-01 22:17:30 -0300

    update name

--- ferrovias/src/app/app.component.spec.ts
+++ ferrovias/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'onda' title`, () => {
+  it(`should have the 'ferrovias' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('onda');
+    expect(app.title).toEqual('ferrovias');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, onda');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ferrovias');
   });
 });
