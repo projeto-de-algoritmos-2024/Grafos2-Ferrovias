ace5d2a618c1cf0b89051a0579490a9fcbe09ec2
Víctor Augusto de Sousa Câmara <victor.camara.va@gmail.com>
2024-12-02 18:28:06 -0300

    Update funcoes.ts

--- funcoes.ts
+++ funcoes.ts
@@ -37,6 +37,30 @@ function dijkstra(graph: Graph, startNode: string): { [key: string]: number } {
     return distances;
 }
 
+function haversine(
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number
+): number {
+  const toRadians = (degree: number) => (degree * Math.PI) / 180;
+
+  const R = 6371; // Raio da Terra em quilômetros
+  const φ1 = toRadians(lat1);
+  const φ2 = toRadians(lat2);
+  const Δφ = toRadians(lat2 - lat1);
+  const Δλ = toRadians(lon2 - lon1);
+
+  const a =
+    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return R * c; // Distância em quilômetros
+}
+
+
 function calcularHipotenusa(catetoOposto: number, catetoAdjacente: number): number {
     return Math.sqrt(catetoOposto ** 2 + catetoAdjacente ** 2);
 }
